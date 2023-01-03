9fde5a987887c4388d8f8e477f03b29e91d58003
jerry8534 <jerryjjinu@gmail.com>
2023-01-03 09:42:01 +0900

    build

---
+++ src/main/java/com/luckytree/shop_service/ShopServiceApplication.java
@@ -0,0 +1,13 @@
+package com.luckytree.shop_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopServiceApplication.class, args);
+	}
+
+}
