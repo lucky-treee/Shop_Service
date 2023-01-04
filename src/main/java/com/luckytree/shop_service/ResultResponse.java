dd9b1968ea31540333cd85fa5487a84de90e75b5
gowjr207@gmail.com <gowjr207@gmail.com>
2023-01-04 21:10:52 +0900

    [FEAT] 응답객체 구현

---
+++ src/main/java/com/luckytree/shop_service/ResultResponse.java
@@ -0,0 +1,28 @@
+package com.luckytree.shop_service;
+
+import jakarta.annotation.Nullable;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@NoArgsConstructor
+public class ResultResponse<T> {
+
+    private int status;     // http 상태값
+    private int code;       // 에러 코드
+
+    private String message; // 메시지
+
+    private T result;       // 전송 데이터
+
+    public ResultResponse(T result) {
+        this.status = HttpStatus.OK.value();
+        this.message = "success";
+        this.result = result;
+    }
+
+    public ResultResponse(HttpStatus httpStatus) {
+        this.status = httpStatus.value();
+    }
+}
