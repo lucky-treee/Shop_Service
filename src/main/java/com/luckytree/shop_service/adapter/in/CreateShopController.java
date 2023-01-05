678d80e54df64bf8283f775418e6bd9ac47e2619
gowjr207@gmail.com <gowjr207@gmail.com>
2023-01-05 23:16:22 +0900

    [FEAT] 스웨거 문법 컨트롤러에 적용

--- src/main/java/com/luckytree/shop_service/adapter/in/CreateShopController.java
+++ src/main/java/com/luckytree/shop_service/adapter/in/CreateShopController.java
@@ -3,6 +3,8 @@ package com.luckytree.shop_service.adapter.in;
 import com.luckytree.shop_service.application.port.in.CreateShopUseCase;
 import com.luckytree.shop_service.application.port.in.RequestShopRegistration;
 import com.luckytree.shop_service.common.dto.ResultResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "샵 등록", description = "샵 등록 관련 API 입니다.")
 @RestController
 @RequestMapping("/shop/create")
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ public class CreateShopController {
 
     private final CreateShopUseCase createShopUseCase;
 
+    @Operation(summary = "샵 등록요청 API")
     @PostMapping("/request")
     public ResultResponse requestShopRegistration(@Valid RequestShopRegistration requestShopRegistration) {
         createShopUseCase.requestShopRegistration(requestShopRegistration);
