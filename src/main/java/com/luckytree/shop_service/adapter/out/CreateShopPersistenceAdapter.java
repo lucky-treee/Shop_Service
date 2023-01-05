a49f5baf6c84e80a875e23afef0bce34303ca18f
gowjr207@gmail.com <gowjr207@gmail.com>
2023-01-05 22:53:08 +0900

    [STYLE] 영속성 어댑터에 Repository 어노테이션 추가

--- src/main/java/com/luckytree/shop_service/adapter/out/CreateShopPersistenceAdapter.java
+++ src/main/java/com/luckytree/shop_service/adapter/out/CreateShopPersistenceAdapter.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @RequiredArgsConstructor
+@Repository
 public class CreateShopPersistenceAdapter implements CreateShopPort {
 
     private final ShopDetailRepository shopDetailRepository;
