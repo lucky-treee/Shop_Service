cf32924a8f6a7d41fabce44981f1a3f3bcfaed21
jerry8534 <jerryjjinu@gmail.com>
2023-01-18 23:25:29 +0900

    [FEAT] shop list 조회를 위한 현재 좌표 표시 객체 생성

---
+++ src/main/java/com/luckytree/shop_service/shop/application/port/in/RequestShopListSelect.java
@@ -0,0 +1,18 @@
+package com.luckytree.shop_service.shop.application.port.in;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RequestShopListSelect {
+
+    @NotNull
+    private double mapX;
+
+    @NotNull
+    private double mapY;
+}
