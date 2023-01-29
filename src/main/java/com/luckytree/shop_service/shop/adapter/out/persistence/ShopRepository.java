10f86d2ceeec636448f86f151c6caab42f279b2c
InHee <ihcho620@gmail.com>
2023-01-29 19:27:19 +0900

    [FEAT] 샵 수정 정보 등록 기능 구현.

--- src/main/java/com/luckytree/shop_service/shop/adapter/out/persistence/ShopRepository.java
+++ src/main/java/com/luckytree/shop_service/shop/adapter/out/persistence/ShopRepository.java
@@ -4,10 +4,13 @@ import com.luckytree.shop_service.shop.domain.ShopStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ShopRepository extends JpaRepository<ShopEntity, Long> {
 
     List<ShopEntity> findByCategoryAndStatus(String category, ShopStatus shopStatus);
 
     List<ShopEntity> findByStatusAndLatLessThanEqualAndLatGreaterThanEqualAndLngLessThanEqualAndLngGreaterThanEqual(ShopStatus shopStatus, double maxLat, double minLat, double maxLng, double minLng);
+
+    Optional<ShopEntity> findByNameAndAddress(String name, String address);
 }
