b142a8877dcc68194a7ae3c36c1a64cec3ddabc0
gowjr207@gmail.com <gowjr207@gmail.com>
2023-02-01 21:15:13 +0900

    [CHORE] 수정기능 관련 클래스 제거

--- src/main/java/com/luckytree/shop_service/shop/application/port/in/UpdateShopUseCase.java
+++
@@ -1,6 +0,0 @@
-package com.luckytree.shop_service.shop.application.port.in;
-
-public interface UpdateShopUseCase {
-
-    void registerShopUpdate(ShopRequest shopRequest,String name,String address);
-}
