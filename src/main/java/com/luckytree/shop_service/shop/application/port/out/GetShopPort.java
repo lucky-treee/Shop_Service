db2055b397ba434765f8e8dd94ea3779ac2e2618
jerry8534 <jerryjjinu@gmail.com>
2023-02-01 18:14:13 +0900

    [STYLE] GetShopPort 반환형 및 메서드 이름 변경

--- src/main/java/com/luckytree/shop_service/shop/application/port/out/GetShopPort.java
+++ src/main/java/com/luckytree/shop_service/shop/application/port/out/GetShopPort.java
@@ -1,6 +1,6 @@
 package com.luckytree.shop_service.shop.application.port.out;
 
-import com.luckytree.shop_service.shop.adapter.out.persistence.ShopTempEntity;
+import com.luckytree.shop_service.shop.adapter.out.persistence.ShopEntity;
 import com.luckytree.shop_service.shop.domain.ShopDetail;
 import com.luckytree.shop_service.shop.domain.ShopSummary;
 
@@ -11,5 +11,5 @@ public interface GetShopPort {
      List<ShopSummary> getShopSummaryByCategory(String category);
 
      List<ShopSummary> getShopAll(double maxLat, double minLat, double maxLng, double minLng);
-     ShopTempEntity getShopTempEntityById(Long shopId);
+     ShopDetail getShopEntityById(Long shopId);
 }
